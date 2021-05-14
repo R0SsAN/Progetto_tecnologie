5c485ecb8187e36cd21ba0a616db4c8eb5317629
acryd03 <66002210+acryd03@users.noreply.github.com>
2021-05-14 20:19:13 +0200

    Implementazione codice classe utenti

--- Progetto_Centralina_Java/src/progetto_centralina_java/Utenti.java
+++ Progetto_Centralina_Java/src/progetto_centralina_java/Utenti.java
@@ -5,10 +5,44 @@
  */
 package progetto_centralina_java;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Luca
+ * @author Andrea
  */
 public class Utenti {
+    //lista di utenti
+    private ArrayList<Utente> listaUtenti;
+    //utente attivo in questo momento
+    public Utente corrente;
+    
+    public Utenti() {
+        listaUtenti=new ArrayList<Utente>();
+        corrente = null;
+    }
+    //singleton
+    static Utenti _instance=null;
+    static synchronized public Utenti getInstance()
+    {
+        if(_instance==null)
+            _instance=new Utenti();
+        return _instance;
+    }
+   
+   public boolean aggiungiUtente(String username, String password){
+       boolean presente=false;
+       for(Utente utente : listaUtenti)
+       {
+           if(utente.getUsername().equals(username)){
+               presente=true;
+           }
+       }
+       if(presente==false){
+           Utente utenteNuovo=new Utente(username,password);
+           listaUtenti.add(utenteNuovo);
+       }
+       return presente;
+   }
     
 }
