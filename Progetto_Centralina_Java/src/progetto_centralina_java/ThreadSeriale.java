be0147241fb2819fecc7e4dcb17a5461bc791d3f
Paolo Terraneo <therealpaolo813@gmail.com>
2021-05-15 14:54:41 +0200

    Implementazione ThreadSeriale
    Thread che riceve una variabile di tipo string da Arduino.
    E imposta i valori all'utente attuale.

--- Progetto_Centralina_Java/src/progetto_centralina_java/ThreadSeriale.java
+++ Progetto_Centralina_Java/src/progetto_centralina_java/ThreadSeriale.java
@@ -5,10 +5,47 @@
  */
 package progetto_centralina_java;
 
+import com.fazecast.jSerialComm.SerialPort;
+import static java.lang.Integer.parseInt;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Luca
  */
-public class ThreadSeriale {
+public class ThreadSeriale extends Thread {
+    Thread thread = new Thread();
+    SerialPort portaSeriale;
+    Utenti utenti;
+
+    //costruttore con la porta seriale
+    ThreadSeriale(SerialPort p) {
+        portaSeriale = p;
+        utenti=Utenti.getInstance();
+    }
+
     
+    public void run() {
+        Scanner scanner = new Scanner(portaSeriale.getInputStream());
+        while (true) {
+            while (scanner.hasNextLine()) {
+                try {
+                    String valoreLetto = scanner.nextLine();
+                    String[] vettore=valoreLetto.split("-");
+                    utenti.corrente.valoreLuce=parseInt(vettore[0]);
+                    utenti.corrente.distanza1=parseInt(vettore[1]);
+                    utenti.corrente.distanza2=parseInt(vettore[2]);
+                } catch (Exception e) {
+                    System.out.println("Errore generato");
+                }
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ThreadSeriale.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
