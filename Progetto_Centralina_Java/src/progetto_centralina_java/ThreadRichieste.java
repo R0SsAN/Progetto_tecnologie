50c0e4a88ffe4eb0f1f0684d140e6e403d45d61a
acryd03 <66002210+acryd03@users.noreply.github.com>
2021-05-16 23:28:54 +0200

    Implementazione thread richieste
    Ho implementato il thread che gestisce le richieste all'api e aggiorna le variabili dell'utente corrente

---
+++ Progetto_Centralina_Java/src/progetto_centralina_java/ThreadRichieste.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progetto_centralina_java;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.IOUtils;
+
+/**
+ *
+ * @author Andrea Puglisi
+ */
+public class ThreadRichieste extends Thread{
+    Utenti listaUtenti;
+
+    public ThreadRichieste() {
+        listaUtenti=Utenti.getInstance();
+    }
+
+    @Override
+    public void run() 
+    {
+        try {
+            riceviUpdate();
+        } catch (IOException ex) {
+            Logger.getLogger(ThreadRichieste.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadRichieste.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void riceviUpdate() throws MalformedURLException, IOException
+    {
+        URL url = new URL("http://194.87.139.81/api/updates.php?action=get_update");
+        InputStream is = url.openStream();
+        try {
+            String risultato = IOUtils.toString(is, StandardCharsets.UTF_8);
+            System.out.println(risultato);
+            
+            
+            ObjectMapper mapper = new ObjectMapper();
+            Messaggio messaggio = mapper.readValue(risultato, Messaggio.class);
+            aggiornaUtente(messaggio);
+            
+            
+        } finally {
+          is.close();
+        }
+    }
+    public void inviaRichiesta(String azione, String data) throws IOException
+    {
+        URL url = new URL("http://194.87.139.81/api/updates.php"+azione+data);
+        InputStream is = url.openStream();
+        is.close();
+    }
+    public void aggiornaUtente(Messaggio messaggio)
+    {
+        for (int i = 0; i < messaggio.bottoni.size(); i++) {
+            listaUtenti.corrente.pulsanti[i]=(boolean)messaggio.bottoni.get(i);
+        }
+    }
+
+    public class Messaggio{
+        List bottoni=new ArrayList<>();
+        List scene=new ArrayList<>();
+
+        public Messaggio() {
+        }
+
+        public void setBottoni(List bottoni) {
+            this.bottoni = bottoni;
+        }
+
+        public void setScene(List scene) {
+            this.scene = scene;
+        }
+    }
+    
+}
