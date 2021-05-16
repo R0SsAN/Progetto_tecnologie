820f9ca84e1883f8f95129adf27bdbe54d2a27c9
acryd03 <66002210+acryd03@users.noreply.github.com>
2021-05-16 11:13:48 +0200

    Implementazione classe InvioTelegram
    Ho implementato la classe InvioTelegram che serve per inviare dei messaggi ad un bot telegram nel caso di intrusione

---
+++ Progetto_Centralina_Java/src/progetto_centralina_java/InvioTelegram.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progetto_centralina_java;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ *
+ * @author Andrea
+ */
+public class InvioTelegram extends TelegramLongPollingBot {
+    
+    Utenti utenti;
+
+    public InvioTelegram() {
+        utenti = Utenti.getInstance();
+    }
+
+    public String getBotUsername() {
+        return "Invio Telegram";
+    }
+
+    @Override
+    public String getBotToken() {
+        // inserire qui il proprio token
+        return "1846536624:AAGq7nZ-MVRoto4WGTfWxB01DfP56giKz5w";
+    }
+
+    public void inviaAllarme() {
+        if (utenti.corrente.allarme == 1) {
+            SendMessage message = new SendMessage();
+            message.setText("ALLARME INTRUSIONE!");
+            try {
+                sendMessage(message);
+            } catch (TelegramApiException ex) {
+                Logger.getLogger(InvioTelegram.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    @Override
+    public void onUpdateReceived(Update update) {
+        //nulla da fare
+    }
+
+}
