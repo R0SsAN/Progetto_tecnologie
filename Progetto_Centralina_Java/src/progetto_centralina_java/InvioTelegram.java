d440987b0bec956efee44517b373a72147b09722
acryd03 <66002210+acryd03@users.noreply.github.com>
2021-05-18 15:13:29 +0200

    Modifica classe invio telegram
    Ho aggiunto il metodo che salva la lista delle chat id in un file

--- Progetto_Centralina_Java/src/progetto_centralina_java/InvioTelegram.java
+++ Progetto_Centralina_Java/src/progetto_centralina_java/InvioTelegram.java
@@ -5,8 +5,16 @@
  */
 package progetto_centralina_java;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -19,9 +27,13 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 public class InvioTelegram extends TelegramLongPollingBot {
     
     Utenti utenti;
+    private final String pathFile= "listaChat.txt";
+    ArrayList<Long> listaChat;
 
     public InvioTelegram() {
         utenti = Utenti.getInstance();
+        listaChat=new ArrayList<Long>();
+        //caricaChat();
     }
 
     public String getBotUsername() {
@@ -36,17 +48,58 @@ public class InvioTelegram extends TelegramLongPollingBot {
 
     public void inviaAllarme() {
         //commento per ricordare allarme
-        SendMessage message = new SendMessage();
-        message.setText("ALLARME INTRUSIONE!");
-        try {
-            sendMessage(message);
-        } catch (TelegramApiException ex) {
-            Logger.getLogger(InvioTelegram.class.getName()).log(Level.SEVERE, null, ex);
+        for (int i = 0; i < listaChat.size(); i++) {
+            SendMessage message = new SendMessage().setChatId(listaChat.get(i));
+            message.setText("ALLARME INTRUSIONE!");
+            try {
+                sendMessage(message);
+            } catch (TelegramApiException ex) {
+                Logger.getLogger(InvioTelegram.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+        
     }
     @Override
     public void onUpdateReceived(Update update) {
-        //nulla da fare
+        listaChat.add(update.getMessage().getChatId());
+    }
+    public void caricaChat()
+    {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(pathFile));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Utenti.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "File "+pathFile+" non trovato!");
+        }
+        String text = null;
+        try {
+            while ((text = br.readLine()) != null){
+                listaChat.add(Long.parseLong(text));
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Utenti.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Errore nel caricamento del file!");
+        }
+        
+        try {
+            br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Utenti.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Errore nella chiusura del file!");
+        }
+    }
+    public void salvaChat()
+    {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathFile))){
+            for (Long id : listaChat) {
+                bw.append(id.toString());
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Impossibile salvare!");
+        }
     }
 
 }
